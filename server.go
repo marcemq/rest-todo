d22fd283609c7e372cbc1ae39285ef0d105a5c89
Morales Quispe, Marcela <marcela.morales.quispe@gmail.com>
2018-06-16 20:01:07 -0500

    Added unit tests for create todo controller.

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ func main() {
 	r.POST("/todo", tc.CreateTodo)
 	r.DELETE("/todo/:id", tc.DeleteTodo)
 
-	err := http.ListenAndServe("0.0.0.0:8080", r)
+	err := http.ListenAndServe(controllers.SRVADDR, r)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
